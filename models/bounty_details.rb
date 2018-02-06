5a097f8c3d3daaf2f3728a502fbdb99db64aa562
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-06 15:08:10 +0000

    Add delete method to the Bounty class

--- models/bounty_details.rb
+++ models/bounty_details.rb
@@ -14,10 +14,7 @@ class Bounty
   end
 
   def save()
-    db = PG.connect({
-      dbname: 'space_cowboys',
-      host: 'localhost'
-      })
+    db = PG.connect({dbname: 'space_cowboys',host: 'localhost'})
 
     sql = "INSERT INTO bounty_details (
     name, species, bounty_value, homeworld
@@ -32,6 +29,15 @@ class Bounty
       @id = db.exec_prepared("save", values)[0]["id"].to_i
 
       db.close()
+  end
+
+  def delete()
+    db = PG.connect({dbname: 'space_cowboys',host: 'localhost'})
+    sql = "DELETE FROM bounty_details WHERE id=$1"
+    values = [@id]
+    db.prepare("delete", sql)
+    db.exec_prepared("delete_one", values)
+    db.close()
   end
 
 
